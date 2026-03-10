79c0402b909b2b3776deddc050ab7857c54b65ff
start.vaadin.com <no-reply@vaadin.com>
2026-03-10 17:33:52 +0000

    Generated project

---
+++ src/main/java/fi/sauli/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package fi.sauli.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/fi/sauli/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package fi.sauli.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
