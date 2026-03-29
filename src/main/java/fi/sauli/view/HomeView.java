f07caf9ca82d540f15a79b9e824e8e288ed6c370
SaleSan <sauli.pitkakangas@edu.savonia.fi>
2026-03-29 15:41:39 +0300

    (feat) Created Home-page and added page-specific CSS-definitions

---
+++ src/main/java/fi/sauli/view/HomeView.java
@@ -0,0 +1,28 @@
+package fi.sauli.view;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.Route;
+import fi.sauli.base.ui.MainLayout;
+
+@Route(value = "", layout = MainLayout.class)
+@Menu(title = "Etusivu")
+public class HomeView extends VerticalLayout {
+
+    public HomeView() {
+        addClassName("home-view");
+        H1 title = new H1("Sähköpotkulauta kaluston hallinta");
+        Paragraph text = new Paragraph("Hallinnoi potkulautoja, niiden ominaisuuksia, asemia ja ajoja");
+
+        Button rides = new Button("Hallitse ajoja");
+        rides.addClickListener(e -> rides.getUI().ifPresent(ui -> ui.navigate("rides")));
+
+        Button scooters = new Button("Hallitse potkulautoja");
+        scooters.addClickListener(e -> scooters.getUI().ifPresent(ui -> ui.navigate("scooters")));
+
+        add(title, text, rides, scooters);
+    }
+}
